9776826d4b672c3f61a4d8b9a35dc2162696c5f7
Daniel Barrera <danub25@gmail.com>
2022-12-07 03:22:34 -0300

    filtro por texto y precio

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -24,15 +24,49 @@ export class ProductListComponent implements OnInit  {
   productos: Producto[] = [];
   provinciaSlctd:string =this.route.snapshot.params['id'];
   public cantprod:any;
-  
-  
+  productosFiltrados: Producto[] = [];
+  filtroTexto: string = "";
+  filtroPrecio: number = 0;
+  precioMaximo: number = 1000;
+
   constructor(private productoSrv:ProductoService,private route:ActivatedRoute) {
     productoSrv.getProductos(this.provinciaSlctd).subscribe((data:any) => {
     this.productos = data;
-    this.cantprod= this.productos.length;
+    this.productosFiltrados = data;
+    this.cantprod= this.productosFiltrados.length;
     });
   } 
-  
+
+  onFiltroTextoChange(parametroIngresado: any) {
+    this.productosFiltrados = this.productos.filter((producto) => {
+      if (this.filtroPrecio != 0){
+        // esto significa que el usuario ingreso filtro
+        // tengo que filtrar por nombre y codigo y ademas por precio
+        return (producto.nombre.toLowerCase().includes(parametroIngresado.toLowerCase())
+            || (producto.ean + '').includes(parametroIngresado)) && producto.precio <= this.filtroPrecio;
+      }
+      else{
+        // solo filtro por nombre y por codigo
+        return producto.nombre.toLowerCase().includes(parametroIngresado.toLowerCase())
+          || (producto.ean + '').includes(parametroIngresado);
+      }
+    });
+  }
+  onFiltroPrecioChange(parametroIngresado: any) {
+    this.productosFiltrados = this.productos.filter((producto) => {
+      if (this.filtroTexto != ""){
+        return producto.precio <= parametroIngresado && (
+          producto.nombre.toLowerCase().includes(this.filtroTexto.toLowerCase())
+            || (producto.ean + '').includes(this.filtroTexto)
+        )
+      }
+      else{
+        return producto.precio <= parametroIngresado;
+      }
+    })
+  }
+
+
   ngOnInit(): void {
     
   }
